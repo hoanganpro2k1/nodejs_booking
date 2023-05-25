b3f3e0d88255dc1095c734730d47fadf11aba545
Ân Bùi <97715641+AnCoNy2001@users.noreply.github.com>
2023-05-25 09:14:26 +0700

    #66

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -15,6 +15,19 @@ let getTopDoctorHome = async (req, res) => {
     }
 };
 
+let getAllDoctors = async (req, res) => {
+    try {
+        let doctors = await doctorService.getAllDoctors();
+        return res.status(200).json(doctors);
+    } catch (e) {
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server ...',
+        });
+    }
+};
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
+    getAllDoctors: getAllDoctors,
 };
