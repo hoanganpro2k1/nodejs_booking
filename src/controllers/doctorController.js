cd93e62a53b95ebc8b5a57a6f19e25e885753c5a
Ân Bùi <97715641+AnCoNy2001@users.noreply.github.com>
2023-05-26 11:38:27 +0700

    #69 Tạo Giao Diện Trang Xem Chi Tiết Thông Tin Bác Sĩ Phía Client

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -20,6 +20,33 @@ let getAllDoctors = async (req, res) => {
         let doctors = await doctorService.getAllDoctors();
         return res.status(200).json(doctors);
     } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server ...',
+        });
+    }
+};
+
+let postInforDoctor = async (req, res) => {
+    try {
+        let response = await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response);
+    } catch (e) {
+        console.log(e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from the server ...',
+        });
+    }
+};
+
+let getDetailDoctorById = async (req, res) => {
+    try {
+        let infor = await doctorService.getDatailDoctorById(req.query.id);
+        return res.status(200).json(infor);
+    } catch (e) {
+        console.log(e);
         return res.status(200).json({
             errCode: -1,
             errMessage: 'Error from the server ...',
@@ -30,4 +57,6 @@ let getAllDoctors = async (req, res) => {
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
+    postInforDoctor: postInforDoctor,
+    getDetailDoctorById: getDetailDoctorById,
 };
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -46,7 +46,82 @@ let getAllDoctors = () => {
     });
 };
 
+let saveDetailInforDoctor = (inputData) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing parameter 123',
+                });
+            } else {
+                await db.Markdown.create({
+                    contentHTML: inputData.contentHTML,
+                    contentMarkdown: inputData.contentMarkdown,
+                    description: inputData.description,
+                    doctorId: inputData.doctorId,
+                });
+                resolve({
+                    errCode: 0,
+                    errMessage: 'Save infor doctor success',
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+let getDatailDoctorById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter',
+                });
+            } else {
+                let data = await db.User.findOne({
+                    where: {
+                        id: inputId,
+                    },
+                    attributes: {
+                        exclude: ['password'],
+                    },
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: ['description', 'contentHTML', 'contentMarkdown'],
+                        },
+                        {
+                            model: db.Allcode,
+                            as: 'positionData',
+                            attributes: ['valueEn', 'valueVi'],
+                        },
+                    ],
+                    raw: false,
+                    nest: true,
+                });
+
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+                }
+                if (!data) data = {};
+
+                resolve({
+                    errCode: 0,
+                    data: data,
+                });
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
+    saveDetailInforDoctor: saveDetailInforDoctor,
+    getDatailDoctorById: getDatailDoctorById,
 };
